8e798f7eab26151fbfe6f2787a46cee48dad0da4
fabmedeiros <fabmed@gmail.com>
2015-04-29 14:57:57 -0300

    Cifras criptográficas em Python

---
+++ ciphers/caesar.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from cipher import Cipher
+""" Implementacao da Cifra de Cesar """
+
+
+class Caesar(Cipher):
+    '''
+    Cifra de Cesar
+    '''
+    def __init__(self):
+        self.__letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    def encrypt(self, texto_plano, key=3):
+        '''
+        Retorna o texto_plano cifrado com a cifra de Cesar,
+        utlizando a chave key, cujo padrao e 3.
+        '''
+        alfabeto_cifrado = self.shift_alphabet(self.__letters, key)
+
+        cipher_text = ''
+        texto_plano = self.format_str(texto_plano)
+        for char in texto_plano:
+            if char in self.__letters:
+                idx = self.__letters.find(char)
+                cipher_text += alfabeto_cifrado[idx]
+        return cipher_text
+
+    def decrypt(self, texto_cifrado, key=3):
+        '''
+        Retorna em texto plano o texto_cifrado decifrado com a
+        cifra de Cesar, utilizando a chave key, cujo padrao e 3.
+        '''
+        return self.encrypt(texto_cifrado, -key)
---
+++ ciphers/cipher.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+""" Classe com metodos basicos para cifras classicas """
+
+
+class Cipher(object):
+    """ Classe base para as cifras classicas """
+
+    def format_str(self, text):
+        '''
+        Retorna text sem espacos e em maiusculas
+        '''
+        return text.replace(' ', '').upper()
+
+    def shift_alphabet(self, alphabet, shift):
+        '''
+        Retorna alphabet com deslocamento de valor shift
+        '''
+        return alphabet[shift:] + alphabet[:shift]
