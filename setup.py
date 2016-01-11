695b8433be8d70b9ac331897ffa11a77e411ab3a
fabmedeiros <fabmed@gmail.com>
2016-01-11 16:37:49 -0200

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+﻿from setuptools import setup
+
+setup(name='ciphers',
+      version='0.1',
+      description='Cryptography ciphers and codes',
+      url='https://github.com/fabmedeiros/ciphers/',
+      author='Fábio Medeiros',
+      author_email='fabmed@gmail.com',
+      packages=['ciphers', 'codes'])
