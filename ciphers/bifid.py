cd4d90120392711f3058abb3c857f8033c302707
Fábio Medeiros <fabmed@gmail.com>
2019-01-08 15:48:26 -0200

    Create bifid.py

---
+++ ciphers/bifid.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from cipher import Cipher
+
+class Bifid(Cipher):
+    def encrypt(self, plaintext, alphabet=None):
+        lins = ''
+        cols = ''
+        plaintext = plaintext.upper()
+        matriz = self.create_square(alphabet)
+        for letra in plaintext:
+            for lin in matriz:
+                if letra in lin:
+                    cols += str(lin.find(letra) + 1)
+                    lins += str(matriz.index(lin) + 1)
+        cipher_num = lins + cols
+        ciphertext = ''
+        for i in range(0, len(cipher_num), 2):
+            ref = cipher_num[i:i + 2]
+            ciphertext += matriz[int(ref[0]) - 1][int(ref[1]) - 1]
+        return ciphertext
+
+    def decrypt(self, texto, alphabet=None):
+        num = ''
+        plain = ''
+        texto = texto.upper()
+        matriz = self.create_square(alphabet)
+        for letra in texto:
+            for linha in matriz:
+                if letra in linha:
+                    num += str(matriz.index(linha) + 1) + str(linha.find(letra) + 1)
+        linhas = num[:len(num)//2]
+        colunas = num[len(num)//2:]
+        for i in range(0, len(linhas)):
+            plain += matriz[int(linhas[i]) - 1][int(colunas[i]) - 1]
+        return plain
+
+bifid = Bifid()
+cifrado = bifid.encrypt('OTREMPARTEASDEZ', 'PLNCYZSMUETBXWVQOAJDKRGFHI')
+print(cifrado)
+print(bifid.decrypt(cifrado, 'PLNCYZSMUETBXWVQOAJDKRGFHI'))
