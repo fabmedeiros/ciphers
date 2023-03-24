599409c9316a5d069bc26c6fb102ca565aae4949
Fábio Medeiros <fabmed@gmail.com>
2023-03-24 13:33:07 -0300

    Create teste_playfair.py

---
+++ streamlit/teste_playfair.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.path.insert(0, '..')
+
+import streamlit as st
+from ciphers import Playfair
+
+st.write(
+    '''# TESTE
+
+    Teste da biblioteca Streamlit
+    '''
+)
+
+text = 'WQ EM YK HK CS UP LD FR UP ZP IK TA RK LE TR LS YS QX TR SU EF CS DK GZ'
+cifra = Playfair()
+st.write(text)
+texto = st.text_input('Texto cifrado')
+if st.button('Clique'):
+    st.write(cifra.decrypt(text, 'DRSEU'))
+    st.text('CIFRAGEM')
+    st.write(cifra.decrypt(texto, 'DRSEU'))
